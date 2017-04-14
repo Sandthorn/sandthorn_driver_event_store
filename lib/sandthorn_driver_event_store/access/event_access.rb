bb80dd0302d2177d0ba4435174ed4d9b802bd85d
Morgan Hallgren <morgan.hallgren@servicewell.se>
2017-04-14 22:25:09 +0200

    replaced old event_args with event_data

--- lib/sandthorn_driver_event_store/access/event_access.rb
+++ lib/sandthorn_driver_event_store/access/event_access.rb
@@ -4,10 +4,10 @@ module SandthornDriverEventStore
     # Reads and writes events.
 
     def store_events(events = [])
+
       events = Utilities.array_wrap(events)
       timestamp = Time.now.utc
       stream_name = "#{events.first[:aggregate_type]}-#{events.first[:aggregate_id]}"
-
       event_store_events = events.map do |event|
         build_event(timestamp, event)
       end
@@ -20,11 +20,11 @@ module SandthornDriverEventStore
 
     def events_by_stream_id(stream_id)
       return storage.read_all_events_forward(stream_id).map { |event|
-        event_args = build_event_args event.data, event.type
+        event_data = build_event_data event.data, event.type
         aggregate_id = event.stream_name.split('-',2).last
 
         {
-          event_args:         event_args,
+          event_data:         event_data,
           aggregate_id:       aggregate_id,
           aggregate_version:  event.position+1,
           event_name:         event.type
@@ -48,7 +48,7 @@ module SandthornDriverEventStore
     def build_event(timestamp, event)
       {
         event_type: event[:event_name].to_s,
-        data: build_event_data(event[:event_args]),
+        data: build_data(event[:event_data]),
         event_id: SecureRandom.uuid,
         id: event[:aggregate_id],
         position: event[:aggregate_version]-1,
@@ -56,24 +56,24 @@ module SandthornDriverEventStore
       }
     end
 
-    def build_event_data event_args
+    def build_data event_data
       hash = {}
-      unless event_args[:attribute_deltas].nil?
-        event_args[:attribute_deltas].each do |item|   
+      unless event_data[:attribute_deltas].nil?
+        event_data[:attribute_deltas].each do |item|   
           hash[item[:attribute_name]] = item[:new_value]
         end
       end
       hash.empty? ? nil : hash
     end
 
-    def build_event_args data, method_name
+    def build_event_data data, method_name
       delta = data ? JSON.parse(data.to_json) : []
       
       attribute_deltas = delta.map do |key, value|
         {attribute_name: key, old_value: nil, new_value: value}
       end
 
-      {:method_name=>method_name, :method_args=>[], :attribute_deltas=>attribute_deltas}
+      {:attribute_deltas=>attribute_deltas}
     end
   end
 end
