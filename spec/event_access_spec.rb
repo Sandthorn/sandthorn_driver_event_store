d4217e58a21f477512d9d3b99960a16c22e444a5
Morgan Hallgren <morganh@axis.com>
2018-06-08 16:55:51 +0200

    better naming in spec

--- spec/event_access_spec.rb
+++ spec/event_access_spec.rb
@@ -9,9 +9,9 @@ module SandthornDriverEventStore
 
     let(:db) { Sequel.connect(event_store_url)}
     let(:aggregate_id) { SecureRandom.uuid }
-    let(:class_name) { "Foo"}
+    let(:aggregate_type) { "Foo"}
     let(:aggregate) do
-      aggregate_access.register_aggregate(aggregate_id, class_name)
+      aggregate_access.register_aggregate(aggregate_id, aggregate_type)
     end
     let(:storage) { return event_store_driver.execute { |db| return db; } }
     let(:access) { EventAccess.new(storage) }
@@ -21,13 +21,13 @@ module SandthornDriverEventStore
         {
           aggregate_version: 1,
           aggregate_id: aggregate_id,
-          aggregate_type: class_name,
+          aggregate_type: aggregate_type,
           event_name: "new",
           event_data: {test: "new_data"}
         },{
           aggregate_version: 2,
           aggregate_id: aggregate_id,
-          aggregate_type: class_name,
+          aggregate_type: aggregate_type,
           event_name: "foo",
           event_data: {test: "foo_data"}
         }
@@ -38,7 +38,7 @@ module SandthornDriverEventStore
 
       it "handles both arrays and single events" do
         access.store_events(events[0])
-        events = access.find_events(aggregate_id, class_name)
+        events = access.find_events(aggregate_id, aggregate_type)
         expect(events.length).to eq(1)
       end
 
@@ -55,7 +55,7 @@ module SandthornDriverEventStore
         it "returns correct events" do
           access.store_events(events)
 
-          stored_events = access.find_events(aggregate_id, class_name)
+          stored_events = access.find_events(aggregate_id, aggregate_type)
           expect(stored_events.size).to eq(events.size)
           expect(stored_events).to all(respond_to(:merge))
           stored_events.each { |event|
